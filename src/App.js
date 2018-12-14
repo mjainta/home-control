f0f357ebd6976da7b14d13c7195f558863321b12
Martin Jainta <martin.jainta@tradebyte.com>
2018-12-14 16:28:51 +0100

    First draw

--- src/App.js
+++ src/App.js
@@ -2,24 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { CirclePicker, ChromePicker } from 'react-color';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faIgloo)
+
+const Color = (props) => {
+  return (
+    <div className="card">
+      <div className="card-body bg-primary">
+        asdölfk
+      </div>
+      <div className="card-body">
+        <CirclePicker />
+      </div>
+    </div>
+  )
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h3>Home Control</h3>
+        <hr />
+            <Color />
       </div>
     );
   }
